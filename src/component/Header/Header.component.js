1cd63f200f87034e25b4b293c4d02c259a41a0fb
Cody Snell <codyjsnell@yahoo.com>
2021-03-08 13:38:17 -0600

    adds functional header component

---
+++ src/component/Header/Header.component.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import TeslaLogo from '../../assets/teslaLogoSmall.svg'
+
+import './Header.styles.css'
+
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="header-logo">
+        <img src={TeslaLogo} alt="Tesla Logo" />
+      </div>
+      <div className="header-center">
+        <p>Model S</p>
+        <p>Model 3</p>
+        <p>Model X</p>
+        <p>Model Y</p>
+        <p>Solar Roof</p>
+        <p>Solar Panel</p>
+      </div>
+      <div className="header-right">
+        <p>Shop</p>
+        <p>Tesla Account</p>
+      </div>
+    </div>
+  )
+}
+
+export default Header
