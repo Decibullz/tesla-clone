b2e952069829e7be65b7a1c4981867fdf9d9e138
Cody Snell <codyjsnell@yahoo.com>
2022-01-20 15:53:05 -0600

    corrected button

--- src/component/Button/Button.component.js
+++ src/component/Button/Button.component.js
@@ -4,7 +4,7 @@ import './Button.styles.css'
 
 const Button = ({ imp, text, link }) => {
   return (
-    <div className={`button ${imp == 'secondary' ? 'button-white' : ''}`}>
+    <div className={`button ${imp === 'secondary' ? 'button-white' : ''}`}>
       <a href={link}>{text}</a>
     </div>
   )
